2d2d855b21ab844492deed775dc673e3cbb46c12
VaibhavGaneriwala <vaibhav@ganeriwala.com>
2022-07-28 19:27:58 +0530

    Main File

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 # This is a simple Python script to convert text into handwriting.
 # Copyright (c) 2022 Vaibhav Ganeriwala
+
+# import the required modules
 import pywhatkit as kit
+
+# get the text from the user
 text = input("Enter the text that you wish to convert to Handwriting : ")
-print("Converting to Handwriting...")
-kit.text_to_handwriting(text, rgb=[0, 0, 138])
-print("Done!")
+
+# main function
+def text_to_handwriting(text):
+    """Convert the text to Handwriting"""
+    kit.text_to_handwriting(text, rgb=[0, 0, 138])
+    print("Converting to Handwriting...")
+    print("Done!")
+
+# call the function
+text_to_handwriting(text)
